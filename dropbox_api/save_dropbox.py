ca293cb4499b4b6a68ac72749adbc14c247af475
Priyank Trivedi <priyankt68@gmail.com>
2014-06-10 10:54:02 -0300

    Update savedropboxapi.py

--- dropbox_api/save_dropbox.py
+++ dropbox_api/save_dropbox.py
@@ -1,20 +1,20 @@
-# Include the Dropbox SDK
+
 import dropbox
 
-# Get your app key and secret from the Dropbox developer website
+# YOu can get this from Dropbox developer website for your developer account.
 app_key = 'xx'
 app_secret = 'xx'
 
 flow = dropbox.client.DropboxOAuth2FlowNoRedirect(app_key, app_secret)
 
-# Have the user sign in and authorize this token
+# User authentication
 authorize_url = flow.start()
 print '1. Go to: ' + authorize_url
-print '2. Click "Allow" (you might have to log in first)'
+print '2. Click "Allow" after loggin in'
 print '3. Copy the authorization code.'
-code = raw_input("Enter the authorization code here: ").strip()
+code = raw_input("Enter authorization code here: ").strip()
 
-# This will fail if the user enters an invalid authorization code
+
 access_token, user_id = flow.finish(code)
 
 client = dropbox.client.DropboxClient(access_token)
